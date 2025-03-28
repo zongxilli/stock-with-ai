d1c9f3cff0a134681d0b38561bc4728dd1190b3d
zongxi2014 <zongxi2014@gmail.com>
2025-03-27 22:32:55 -0400

    use full historical data as always

--- app/actions/eodhd/get-compressed-historical-data-for-analysis.ts
+++ app/actions/eodhd/get-compressed-historical-data-for-analysis.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { getHistoricalData } from './get-historical-data';
-import { getHistoricalData1MonthFull } from './get-historical-data-1-month-full';
+// import { getHistoricalData1MonthFull } from './get-historical-data-1-month-full';
 import { TimeRange } from './types/types';
 import { compressHistoricalData } from './utils/compress';
 
@@ -10,24 +10,27 @@ export const getCompressedHistoricalDataForAnalysis = async (
 	exchange: string,
 	range: TimeRange
 ): Promise<string> => {
-	const historicalData = await getHistoricalData(code, exchange, range);
+	const historicalData = await getHistoricalData(
+		code,
+		exchange,
+		range,
+		false
+	);
 	const compressedHistoricalData = compressHistoricalData(
 		historicalData,
-		true,
+		false,
 		code
 	);
 
-	const historicalData1MonthFull = await getHistoricalData1MonthFull(
-		code,
-		exchange
-	);
-	const compressedHistoricalData1MonthFull = compressHistoricalData(
-		historicalData1MonthFull,
-		true,
-		code
-	);
+	// const historicalData1MonthFull = await getHistoricalData1MonthFull(
+	// 	code,
+	// 	exchange
+	// );
+	// const compressedHistoricalData1MonthFull = compressHistoricalData(
+	// 	historicalData1MonthFull,
+	// 	true,
+	// 	code
+	// );
 
-	return (
-		compressedHistoricalData + '\n\n' + compressedHistoricalData1MonthFull
-	);
+	return compressedHistoricalData;
 };
